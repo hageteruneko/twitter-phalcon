5d29916de112d43e4dd6154ec0263cb84518acc8
hageteruneko <39547980+hageteruneko@users.noreply.github.com>
2019-01-11 00:53:01 +0900

    yws

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -32,19 +32,37 @@ class IndexController extends ControllerBase
         $user_connection = new TwitterOAuth(CK, CKS, $access_token['oauth_token'], $access_token['oauth_token_secret']);
         $this->view->connection = $user_connection;
         $user_info = $user_connection->get('account/verify_credentials');
-        
+
         $twitter = new twitter();
         $twitter->access_token = $access_token;
         $twitter->id = $user_info->id;
         $twitter->name = $user_info->name;
         $twitter->screen_name = $user_info->screen_name;
-        $twitter->profile_image = $user_info->profile_image;
+        $twitter->profile_image = $user_info->profile_image_url_https;
         $twitter->zikosyokai = $user_info->description;
+
+        $this->persistent->id = $twitter->id;
+        $this->persistent->name = $twitter->name;
+        $this->persistent->profile_image = $twitter->profile_image;
+        
         //var_dump($user_connection->get("account/verify_credentials"));
+        if ($twitter->save() === false) {
+            echo "できなぁい：\n";
+        
+            $messages = $twitter->getMessages();
+        
+            foreach ($messages as $message) {
+                echo $message, "\n";
+            }
+        } else {
+            echo 'セーブに成功した';
+        }
     }
     public function apliAction()
     {
-
+        $this->view->id = $this->persistent->id;
+        $this->view->name = $this->persistent->name;
+        $this->view->image = $this->persistent->profile_image;
     }
 
 }
